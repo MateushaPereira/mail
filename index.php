374b9a65e67d3f0accdf4460ecd7cc5a9d9d30b0
Mateus H A Pereira <42526696+MateushaPereira@users.noreply.github.com>
2019-05-01 16:17:21 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,18 +3,12 @@
 
 <head>
 	<meta charset="UTF-8">
-	<link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="//assets.locaweb.com.br/locastyle/2.0.6/stylesheets/locastyle.css">
-	<script src="validacao.js"></script>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Document</title>
+	<title>Envio de Email</title>
 </head>
 
 <body>
-	<?php 
-		echo("olha")
-	?>
 	<form action="./email.php" method="POST">
 		<label for="nome">NOME</label> <br>
 		<input type="text" id="nome" name="nome" placeholder="Dogite seu nome completo!" require>
@@ -27,10 +21,5 @@
 		<br>
 		<input type="submit" value="Enviar">
 	</form>
-
-	<script type="text/javascript" src="//code.jquery.com/jquery-2.0.3.min.js"></script>
-	<script type="text/javascript" src="//assets.locaweb.com.br/locastyle/2.0.6/javascripts/locastyle.js"></script>
-	<script type="text/javascript" src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
 </body>
-
 </html>
